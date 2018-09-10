bdfcd31ad935cc371a26d439f21355a48b3e52ea
sergaben <gabense@hotmail.com>
2018-09-10 20:43:07 +0100

    adding store to index.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux'
 import './index.css';
 import App from './App';
+import configure from './store'
+import routes from './routes';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = configure();
+
+ReactDOM.render(
+    <Provider store={store}>
+        <BrowserRouter>
+            {routes}
+        </BrowserRouter>
+    </Provider>
+    , document.getElementById('root'));
 registerServiceWorker();
---
+++ src/routes.js
@@ -0,0 +1,10 @@
+import React from "react";
+import App from "./App";
+import {Route} from "react-router";
+
+export default (
+    <div>
+      {/* Add routes here */}
+      <Route path="/" component={App}/>
+    </div>
+)
